6bc89fd48e26cd2c6cd405bac10666d38d0ca12e
RUSHABH82 <makanirushabh0786@gmail.com>
2024-07-10 13:29:57 +0530

    initial backend push(springboot template)

---
+++ backend-springboot/src/main/java/com/credmarg/admin/employeevendormanagement/EmployeeVendorManagementApplication.java
@@ -0,0 +1,13 @@
+package com.credmarg.admin.employeevendormanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeVendorManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeVendorManagementApplication.class, args);
+	}
+
+}
