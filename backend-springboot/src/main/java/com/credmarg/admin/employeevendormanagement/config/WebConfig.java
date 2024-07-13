b541e54f76ceadc81bc4b10f0bed2fca6af4ca58
RUSHABH82 <makanirushabh0786@gmail.com>
2024-07-13 19:56:49 +0530

    add CorsFilter, fix mail reading issue

--- backend-springboot/src/main/java/com/credmarg/admin/employeevendormanagement/config/WebConfig.java
+++ backend-springboot/src/main/java/com/credmarg/admin/employeevendormanagement/config/WebConfig.java
@@ -1,9 +1,15 @@
 package com.credmarg.admin.employeevendormanagement.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 @EnableWebMvc
@@ -13,4 +19,19 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
     }
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+
+        corsConfig.setAllowedOrigins(List.of("http://localhost:5173"));
+        corsConfig.addAllowedMethod("*");
+        corsConfig.addAllowedHeader("*");
+        corsConfig.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsFilter(source);
+    }
+
 }
